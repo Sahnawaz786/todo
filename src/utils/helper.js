70e1ab7cea4673722b213bdb424b35ebe7862f84
Sahnawaz Alam <52168845+Sahnawaz786@users.noreply.github.com>
2023-08-28 23:44:16 +0530

    Delete src directory

--- src/utils/helper.js
+++
@@ -1,9 +0,0 @@
-export const filter = (arr,data)=>{
-    return arr.filter((elem)=> elem.task!=data);
-}
-
-export const name_provider = (name='Sahnawaz Alam')=>{
-    return `${name[0]}${name[name?.indexOf(' ') + 1]}`
-}
-
-
